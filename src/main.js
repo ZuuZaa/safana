8c7167c2062aef4feb0d1144d0fa40124c6af260
ZuuZaa <zumbaghirova@gmail.com>
2023-06-08 16:02:27 +0400

    footer

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 const header = document.querySelector('.header');
-const menuBtn = document.querySelector('.menu-btn');
 let scrollTopPosition = 0;
 
 // ---- header on scroll behavior ---------
@@ -7,9 +6,32 @@ document.addEventListener("scroll", () => {
   scrollTopPosition = window.scrollY;
   scrollTopPosition > 90 ? header.classList.add('onscroll') : header.classList.remove('onscroll')
 });
-menuBtn.addEventListener("click", () => {
-  header.classList.toggle('collapsed-nav')
-});
 
+const owl = $('.owl-carousel');
+
+owl.owlCarousel({
+  items: 5,
+  loop: true,
+  margin: 10,
+  autoplay: true,
+  autoplayTimeout: 1000,
+  autoplayHoverPause: true,
+  nav: true,
+  rewindNav: true,
+  navText: [$('.control-btn.prev'), $('.control-btn.next')],
+  dots: false,
+  responsive: {
+    0: {
+      items: 2
+    },
+    900: {
+      items: 3
+    },
+    1200: {
+      item: 5
+    }
+  }
+})
+owl.trigger('play.owl.autoplay', [1000])
 
 
